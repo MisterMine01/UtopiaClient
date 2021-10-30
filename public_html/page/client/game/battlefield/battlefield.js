761b2a3dbc44dba483c579592b3ccdab1b51ade0
MisterMine01 <pokemovie06@gmail.com>
2021-10-30 15:28:35 +0200

    Begin battlefield visual
    Cut html in multiple div
    Adding battlefield.css
    begin reload function for the battlefield

--- public_html/page/client/game/battlefield/battlefield.js
+++ public_html/page/client/game/battlefield/battlefield.js
@@ -1,19 +1,33 @@
 Game.Battle = new class {
     constructor() {
         this.game_data = Game.loadbattle.Starter;
+        this.PlayerId = Game_data.BS_server.PlayerId;
+        Ennemyid = Object.keys(new_data);
+        Ennemyid.splice(Ennemyid.indexOf(this.PlayerId), 1);
+        this.EnnemyId = Ennemyid[0];
         setInterval(function () {
             if (page_loaded === "client/game/battlefield") {
                 var data = Game_data.BS_server.GetBattle();
-                if (this.game_data !== data) {
+                if (JSON.stringify(this.game_data) !== JSON.stringify(data)) {
                     console.log("changement");
                 }
             }
         }, 100);
     }
+    
+    reload_Board(board_name, data) {
+        //Adding Board code
+    }
+    
+    reload_battle(new_data) {
+        reload_Board("user_battlefield", new_data[this.PlayerId]);
+        reload_Board("enemy_battlefield", new_data[this.EnnemyId]);
+        //adding hand and info
+    }
 
     sending_battle(card_id, board_id, phase_id) {
         if (phase_id.include(this.game_data["Phase"]["PhaseType"])) {
-            if (self.game_dict["Phase"]["PlayerId"] === Game_data.BS_server.PlayerId) {
+            if (self.game_dict["Phase"]["PlayerId"] === this.PlayerId) {
                 if (self.game_dict["Phase"]["PhaseUser"] === 0) {
                     Game_data.BS_server.SendBattle(null,
                         JSON.stringify({"card": card_id, "board_id": board_id}));
